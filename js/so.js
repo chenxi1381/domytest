dc02281e0bf982d876cb2d7a0b3cfee03906f5d4
chenxi1381 <dufresne@DufresnedeMacBook-Pro.local>
2016-01-17 14:41:27 +0800

    整合

---
+++ js/so.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+	a();
+});
+
+function a() {
+	$(".left_title_text1").click(function() {
+		$(".left_title_text1").text("aaaaaa");
+	});
+
+//	$("#left_title_text1").click(function() {
+//		alert("aa");
+//	});
+$(".left_title_text1").text("bbbbb");
+//	$(".left_title_text1").attr("text", "bbbbb");
+}
+//$(function() {
+//	$("div").each(function() {
+//		$(this).click(function() {
+//			alert($(this).text());
+//		});
+//	});
+//});
